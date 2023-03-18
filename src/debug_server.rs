91e26d9ad082ebd9284982e5bf9c4349490139a2
soruh <mail@soruh.de>
2023-03-18 17:44:42 +0100

    name http worker threads

--- src/debug_server.rs
+++ src/debug_server.rs
@@ -1,3 +1,5 @@
+use futures::Future;
+use hyper::rt::Executor;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Response, Server};
 use std::convert::Infallible;
@@ -6,21 +8,32 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use crate::ports::PortHandler;
+use crate::spawn;
+
+#[derive(Clone)]
+struct NamedExecutor;
+impl<T: Send + 'static, Fut: Future<Output = T> + Send + 'static> Executor<Fut> for NamedExecutor {
+    fn execute(&self, fut: Fut) {
+        spawn("http worker", fut);
+    }
+}
 
 pub async fn debug_server(addr: SocketAddr, port_handler: Arc<Mutex<PortHandler>>) {
-    let server = Server::bind(&addr).serve(make_service_fn(move |_conn| {
-        let port_handler = port_handler.clone();
-        async move {
-            Ok::<_, Infallible>(service_fn(move |_req| {
-                let port_handler = port_handler.clone();
-                async move {
-                    Ok::<_, Infallible>(Response::new(Body::from(
-                        port_handler.lock().await.status_string(),
-                    )))
-                }
-            }))
-        }
-    }));
+    let server = Server::bind(&addr)
+        .executor(NamedExecutor)
+        .serve(make_service_fn(move |_conn| {
+            let port_handler = port_handler.clone();
+            async move {
+                Ok::<_, Infallible>(service_fn(move |_req| {
+                    let port_handler = port_handler.clone();
+                    async move {
+                        Ok::<_, Infallible>(Response::new(Body::from(
+                            port_handler.lock().await.status_string(),
+                        )))
+                    }
+                }))
+            }
+        }));
 
     // Run this server for... forever!
     if let Err(e) = server.await {
